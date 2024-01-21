4f99f193c186ed95af663eee1c21014647fd4320
Alan <alanjulianrecalde@gmail.com>
2024-01-20 21:45:56 -0300

    cambio 2

--- script.js
+++ script.js
@@ -81,7 +81,7 @@ btnHold.addEventListener('click', function () {
 
 
         // 2. Check if player's score is >= 100
-        if (scores[activePlayer] >= 20) {
+        if (scores[activePlayer] >= 100) {
             // finish the game
             playing = false;
             document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
